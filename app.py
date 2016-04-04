52206a45df5d24221edea9e1e8282b60d798a4ef
gsganden <greg@gandenberger.org>
2016-04-04 17:34:02 -0400

    See if querying works on heroku

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import requests
 import pandas as pd
 from datetime import datetime
 from bokeh.plotting import figure, output_notebook, output_file, save
+import os
+import psycopg2
+import urlparse
 
 app = Flask(__name__)
 
@@ -15,11 +18,29 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     else:
-        pitcher = request.form['pitcher']
-        image_file = pitcher.lower()
-        image_file = image_file.split()
-        image_file = '_'.join(image_file) + '.png'
-        return render_template('results.html', pitcher = pitcher, image_file = image_file)
+        return render_template('results.html', result = run_query("SELECT * FROM pitches LIMIT 10;"))
+
+def run_query(query):
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+
+    cur = conn.cursor()
+
+    cur.execute(query)
+    result = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return result
 
 if __name__ == '__main__':
     app.run(port=33508)
