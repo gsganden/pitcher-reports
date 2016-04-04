4d446f6b810fdb1a996ab9b65259c1212c6b951a
gsganden <greg@gandenberger.org>
2016-04-04 17:15:45 -0400

    Test functional version of connecting to postgres

--- connect_to_postgres.py
+++ connect_to_postgres.py
@@ -2,23 +2,49 @@ import os
 import psycopg2
 import urlparse
 
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
+# urlparse.uses_netloc.append("postgres")
+# url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+# conn = psycopg2.connect(
+#     database=url.path[1:],
+#     user=url.username,
+#     password=url.password,
+#     host=url.hostname,
+#     port=url.port
+# )
 
+# cur = conn.cursor()
 
+# cur.execute("SELECT * FROM pitches LIMIT 10;")
+# print cur.fetchone()
 
-cur = conn.cursor()
+# cur.close()
+# conn.close()
 
-cur.execute("SELECT * FROM pitches LIMIT 10;")
-print cur.fetchone()
+def run_query(query):
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-cur.close()
-conn.close()
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+
+    cur = conn.cursor()
+
+    cur.execute(query)
+    result = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return result
+
+query = "SELECT * FROM pitches LIMIT 10;"
+
+query_result = run_query(query)
+
+print query_result
